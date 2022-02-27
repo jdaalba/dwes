8eff248a860ed54f4570501ab9caf619ed2bdf68
jdaalba <jdaalba@gmail.com>
2022-02-27 20:28:41 +0100

    Práctica 7

--- p7/const.php
+++ p7/const.php
@@ -1,7 +1,7 @@
 <?php
 
-const SERVER = "sql300.byethost11.com";
-const SCHEMA = "b11_30764485_p6";
-const USER = "b11_30764485";
-const PASS = "ssAXec5Ugt4p8Tv";
-const API_SERVER = "http://www.jdaalba.byethost11.com";
+const SERVER = "*******";
+const SCHEMA = "*******";
+const USER = "*******";
+const PASS = "*******";
+const API_SERVER = "*******";
